ab3cc3d3146de5b729d42c5fdfbc57b044d61436
Sydorska <sydorska.l@gmail.com>
2025-11-21 21:51:32 -0700

    Homework for lesson 3

---
+++ homework/eugene_okulik/Lesson_3/Home_work3.4.py
@@ -0,0 +1,8 @@
+# Даны катеты прямоугольного треугольника. Найти его гипотенузу и площадь
+
+leg1 = 3
+leg2 = 2
+hypotenuse = (leg1 ** 2 + leg2 ** 2) ** 0.5
+triangle_s = (leg1 * leg2) / 2
+print(hypotenuse)
+print(triangle_s)
