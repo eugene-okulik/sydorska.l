408d74ae7e73e7181ab36934f21e720b0214397c
Sydorska <sydorska.l@gmail.com>
2025-11-28 17:50:41 -0700

    Sydorska lesson6 updated HW

--- homework/eugene_okulik/Lesson_6/HW_6.1.py
+++ homework/eugene_okulik/Lesson_6/HW_6.1.py
@@ -1,4 +1,5 @@
-text = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
+text = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. n\
+Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
 words = text.split()
 new_text = []
 for word in words:
--- homework/eugene_okulik/Lesson_6/HW_6.2.py
+++ homework/eugene_okulik/Lesson_6/HW_6.2.py
@@ -1,11 +1,10 @@
 numbers = range(1, 101)
 for n in numbers:
-    if n%3 == 0 and n%5 == 0:
+    if n % 3 == 0 and n % 5 == 0:
         print('FuzzBuzz')
-    elif n%3 == 0:
+    elif n % 3 == 0:
         print('Fuzz')
-    elif n%5 == 0:
+    elif n % 5 == 0:
         print('Buzz')
     else:
         print(n)
-        
