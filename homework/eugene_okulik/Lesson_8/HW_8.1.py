9b3f0b921262042ddcfca6776b0e477308badf18
Sydorska <sydorska.l@gmail.com>
2025-12-01 14:56:19 -0700

    updated Sydorska HW

--- homework/eugene_okulik/Lesson_8/HW_8.1.py
+++ homework/eugene_okulik/Lesson_8/HW_8.1.py
@@ -1,9 +1,11 @@
 import random
+
+
 def pay_sum(salary, bonus):
     if bonus is True:
-       bonus_to_pay = int(random.randint(250, 750))
-       total_pay = salary + bonus_to_pay
-       print(f'{salary}, {bonus} - \'${total_pay}\'')
+        bonus_to_pay = int(random.randint(250, 750))
+        total_pay = salary + bonus_to_pay
+        print(f'{salary}, {bonus} - \'${total_pay}\'')
     else:
         print(f'{salary}, {bonus} - \'${salary}\'')
 
