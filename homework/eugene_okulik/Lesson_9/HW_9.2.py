cdbee679761748a33c539b2681221d0441df0e69
Sydorska <sydorska.l@gmail.com>
2025-12-02 14:23:47 -0700

    updated 9.2

--- homework/eugene_okulik/Lesson_9/HW_9.2.py
+++ homework/eugene_okulik/Lesson_9/HW_9.2.py
@@ -1,5 +1,9 @@
-temperatures = [20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27, 22, 22, 23, 25, 29, 29, 31, 33, 31, 30, 32, 30, 28, 24, 23]
-hot_temp = list(filter(lambda temp : temp > 28, temperatures))
+temperatures = [
+    20, 15, 32, 34, 21, 19, 25, 27, 30, 32,
+    34, 30, 29, 25, 27, 22, 22, 23, 25, 29,
+    29, 31, 33, 31, 30, 32, 30, 28, 24, 23
+]
+hot_temp = list(filter(lambda temp: temp > 28, temperatures))
 print(max(hot_temp))
 print(min(hot_temp))
-print(round((sum(hot_temp))/len(hot_temp), 2))
+print(round((sum(hot_temp)) / len(hot_temp), 2))
